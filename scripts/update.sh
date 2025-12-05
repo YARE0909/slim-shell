0d08d3fd8a0728525f2def252aa8555d54c6928d
YARE0909 <pradyumnad092004@gmail.com>
2025-12-06 01:27:33 +0530

    Minor change

--- scripts/update.sh
+++ scripts/update.sh
@@ -25,21 +25,6 @@ git pull --rebase
 echo "[*] Rebuilding..."
 make
 
-OS="$(uname -s)"
-
-case "$OS" in
-    Linux*|Darwin*)
-        echo "[*] Installing updated binary..."
-        sudo cp slim /usr/local/bin/slim
-        sudo chmod +x /usr/local/bin/slim
-        ;;
-    MINGW*|MSYS*|CYGWIN*)
-        echo "[*] Updating Windows binary..."
-        mkdir -p "$HOME/bin"
-        cp slim.exe "$HOME/bin/slim.exe"
-        ;;
-esac
-
 echo ""
 echo "--------------------------------------"
 echo "   Slim Shell is now up to date!"
